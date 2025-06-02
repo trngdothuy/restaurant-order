4ab4545caf2361ab02b10bf5e0dca3a4823d1f77
Trang <dothuytrang2312@gmail.com>
2025-06-02 18:34:51 +0200

    update port

--- backend/app.py
+++ backend/app.py
@@ -33,5 +33,5 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context():
          db.create_all()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=True,host="0.0.0.0", port=port)
